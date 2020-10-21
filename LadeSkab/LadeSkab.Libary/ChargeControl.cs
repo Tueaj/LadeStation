c9292aa56af8add82d43915265fd066f6024a179
Tueaj <47721101+Tueaj@users.noreply.github.com>
2020-10-21 11:34:53 +0200

    push event. så alle kan se navngivning

--- LadeSkab/LadeSkab.Libary/ChargeControl.cs
+++ LadeSkab/LadeSkab.Libary/ChargeControl.cs
@@ -1,12 +1,59 @@
-﻿using Ladeskab.Libary.interfaces;
+﻿using System;
+using Ladeskab.Libary.interfaces;
 
 namespace Ladeskab.Libary
 {
     public class ChargeControl : IChargeControl
     {
+        public IUsbCharger UsbCharger
+        {
+            get
+            {
+                return UsbCharger;
+            }
+            set
+            {
+                UsbCharger = value;
+                UsbCharger.CurrentValueEvent += HandleCurrentEvent;
+            }
+        }
+
+        private void HandleCurrentEvent(object sender, CurrentEventArgs e)
+        {
+            if (e.Current == 0)
+            {
+                isConnected = false;
+            }
+            else if (0<e.Current && e.Current<=5)
+            {
+                isConnected = true;
+
+            }
+            else if (5 < e.Current && e.Current <= 500)
+            {
+                isConnected = true;
+            }
+            else if (500 < e.Current)
+            {
+                isConnected = true;
+            }
+
+            ChargerConnectedChange();
+        }
+
+        private void ChargerConnectedChange()
+        {
+            ChargerConnectionValueEvent?.Invoke(this, new ChargerConnectionValue() { ChargerConnected = this.isConnected });
+        }
+
+        public event EventHandler<ChargerConnectionValue> ChargerConnectionValueEvent;
+        public IDisplay Display { get; set; }
+        
+        private bool isConnected { get; set; }
+
         public bool IsConnected()
         {
-            throw new System.NotImplementedException();
+            return isConnected;
         }
 
         public void StartCharge()
--- LadeSkab/LadeSkab.Libary/Interfaces/IChargeControl.cs
+++ LadeSkab/LadeSkab.Libary/Interfaces/IChargeControl.cs
@@ -1,7 +1,15 @@
-﻿namespace Ladeskab.Libary.interfaces
+﻿using System;
+
+namespace Ladeskab.Libary.interfaces
 {
+    public class ChargerConnectionValue : EventArgs
+    {
+        public bool ChargerConnected { set; get; }
+    }
     public interface IChargeControl
     {
+        event EventHandler<ChargerConnectionValue> ChargerConnectionValueEvent;
+        public IDisplay Display { get; set; }
         public bool IsConnected();
         public void StartCharge();
         public void StopCharge();
