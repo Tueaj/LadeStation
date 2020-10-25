5d57d587cace494df1b9799d1323ccede5de8eef
AUJacobNyvang <70698246+AUJacobNyvang@users.noreply.github.com>
2020-10-25 21:29:23 +0100

    Display Test PrintConnectPhone

---
+++ LadeSkab/LadeSkab.Test/TestDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Ladeskab.Libary;
+using NUnit.Framework;
+
+namespace Ladeskab.Test
+{
+    [TestFixture]
+    public class TestDisplay
+    {
+        private Display _uut;
+        private StringWriter SW;
+
+        [SetUp]
+        public void setup()
+        {
+            SW = new StringWriter();
+            _uut = new Display();
+            Console.SetOut(SW);
+        }
+
+        [Test]
+        public void PrintConnectPhone_IsPrinted()
+        {
+            _uut.PrintConnectPhone();
+            string stringRes = "Charger is ready\r\nConnect Phone to Charger\r\n";
+            Assert.That(stringRes, Is.EqualTo(SW.ToString()));
+        }
+
+    }
+}
