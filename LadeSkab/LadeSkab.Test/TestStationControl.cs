bb2acabb154bfbc585c13364beada473b4c71257
AsgerHolm <47742247+AsgerHolm@users.noreply.github.com>
2020-10-25 20:00:15 +0100

    Begyndende test af stationcontrol

--- LadeSkab/LadeSkab.Test/TestStationControl.cs
+++ LadeSkab/LadeSkab.Test/TestStationControl.cs
@@ -7,20 +7,26 @@ using System.Threading;
 using System.Threading.Tasks;
 using Ladeskab.Libary;
 using Ladeskab.Libary.interfaces;
+using NSubstitute;
 
 namespace Ladeskab.Test
 {
+    [TestFixture]
     class TestStationControl
     {
         private StationControl _uut;
+        private readonly TestDoorSource _doorSource;
+        private readonly TestChargerSource _chargeControlSource;
         [SetUp]
         public void Setup()
         {
-            IChargeControl charger = new ChargeControl();
-            _uut = new StationControl(charger);
-
-            IDoor door = new Door();
-            _uut = new StationControl(door);
+            _doorSource = Substitute.For<IDoor>();
+            _chargeControlSource = Substitute.For<IChargeControl>();
+            _uut = new StationControl(_doorSource,_chargeControlSource);
         }
+
+
+
+        
     }
 }
