3055791ec8025d01297d40786b3d59852d82dfa2
Tueaj <47721101+Tueaj@users.noreply.github.com>
2020-10-25 21:25:45 +0100

    FakeRfid test made.

---
+++ LadeSkab/LadeSkab.Test/TestRfidReader.cs
@@ -0,0 +1,43 @@
+﻿using Ladeskab.Libary;
+using Ladeskab.Libary.interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Test
+{
+    public class TestRfidReader
+    {
+        private IRfidReader _uut;
+        private bool RfidEventRaised{ get; set; }
+
+        private int IDFromEvent{ get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new FakeRfidReader();
+            RfidEventRaised = false;
+            _uut.RFIDDetectedEvent += (sender, args) =>
+            {
+                RfidEventRaised = true;
+                IDFromEvent = args.RFID;
+            };
+        }
+
+        [Test]
+        public void RFIDDetectedAndEventRaised()
+        {
+            int TestId = 1234;
+            _uut.ScanRFID(TestId);
+            Assert.IsTrue(RfidEventRaised);
+        }
+
+        [Test] public void RFIDDetectedAndIdSendWithEvent()
+        {
+            int TestId = 1234;
+            _uut.ScanRFID(TestId);
+            Assert.That(TestId.Equals(IDFromEvent));
+        }
+
+    }
+}
